7e0df23835ca276cd3245c9f2139c0115a2887ea
Gene-Weaver <willwe@umich.edu>
2025-03-05 19:12:45 -0500

    auth

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
 
+'''
+python -m build
+python -m twine upload dist/* --verbose
+'''
+
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
